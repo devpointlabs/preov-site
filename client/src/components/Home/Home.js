a48e57c30209e42479119bb4ed5fa2d3e8dcb4d8
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-25 17:20:40 -0600

    HomeHero in progress

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+// stack all Home layout components here 
+
+import React from "react"
+import axios from "axios"
+// import { Container, } from "semantic-ui-react"
+
+const Home = () => (
+  <>
+
+  </>
+)
+
+export default Home;
