1949849bbeaaeff5c5229cb9abf709fabd007e84
Willard Cron <willardcron@gmail.com>
2019-06-25 14:57:50 -0600

    navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link, } from 'react-router-dom';
+import { Menu, } from "semantic-ui-react";
+
+const NavBar = () => (
+  <Menu>
+    <Link to="/">
+      <Menu.Item>
+        Home
+      </Menu.Item>
+    </Link>
+    <Link to="/about">
+      <Menu.Item>
+        About Us
+      </Menu.Item>
+    </Link>
+    <Link to="/howitworks">
+      <Menu.Item>
+      How It Works
+      </Menu.Item>
+    </Link>
+    <Link to="/blogs">
+      <Menu.Item>
+      Blog
+      </Menu.Item>
+    </Link>
+  </Menu>
+)
+
+export default NavBar;
