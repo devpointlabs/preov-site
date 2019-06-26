26c940b4164a5065e920629a6ac2c99102488c2b
Willard Cron <willardcron@gmail.com>
2019-06-26 15:12:46 -0600

    About Us component frame

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import AboutUs from './components/AboutUs';
+import { Switch, Route, } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      
+      <Route exact path="/AboutUs" component={AboutUs} />
     </div>
   );
 }
---
+++ client/src/components/AboutUs.js
@@ -0,0 +1,35 @@
+// Origin story <div>
+// MEET THE TEAM grid
+
+import React from 'react'
+import { Container, Grid, Image, Divider, Header, Segment } from "semantic-ui-react";
+const AboutUs = () => {
+
+
+
+return (
+  <>
+  <Container text >
+    <Segment>
+      <Header as='h1'>
+      {/* <Image circular src='images/preOV'/> */}
+        About Us
+      </Header>
+      <h3> Our Story </h3>
+    <div>
+      <p> The idea for PreOv was conceived one late evening in Sugar House Coffee,
+        over hot tea, cookies, and a shared passion for helping women better
+        understand their bodies without the guesswork and hassle of daily testing.</p>
+      <p>
+        PreOv is a female-led comapny focused on empowering women and couples in all
+        stages of life.
+      </p>
+
+    </div>
+    </Segment>
+  </Container>
+  </>
+)
+};
+
+export default AboutUs;
