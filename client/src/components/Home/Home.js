131cfa08309fe3a6c09384af6b7149b74ebe842f
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-27 15:15:04 -0600

    ourtech and image updates

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,17 +1,16 @@
 // stack all Home layout components here 
-
 import React from "react"
-// import axios from "axios"
 import HomeHero from "./HomeHero"
 import UnderHero from "./UnderHero"
 import OurStory from "./OurStory"
-// import { Container, } from "semantic-ui-react"
+import OurTech from "./OurTech"
 
 const Home = () => (
   <>
     <HomeHero />
     <UnderHero />
     <OurStory />
+    <OurTech />
   </>
 )
 
