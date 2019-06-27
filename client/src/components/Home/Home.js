0fe4405438c1ae3a7616e2d0bfc039a5e0cb78d4
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-27 12:36:31 -0600

    ourstory component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,12 +4,14 @@ import React from "react"
 // import axios from "axios"
 import HomeHero from "./HomeHero"
 import UnderHero from "./UnderHero"
+import OurStory from "./OurStory"
 // import { Container, } from "semantic-ui-react"
 
 const Home = () => (
   <>
     <HomeHero />
     <UnderHero />
+    <OurStory />
   </>
 )
 
