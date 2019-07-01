4a5ba0c2627c76c6a79313219d9f835dbbc10d71
Nicki Nixon <nickitza@gmail.com>
2019-07-01 15:08:49 -0600

    delete post

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -17,7 +17,15 @@ class Posts extends React.Component {
     return newDate.toDateString();
   };
 
-  adminButtons = () => (
+  deletePost = (id, post) => {
+    axios.delete(`/api/posts/${id}`, post)
+    .then(res => {
+      const {posts} = this.state
+      this.setState({posts: posts.filter(post => post.id !== id)})
+    })
+  }
+
+  adminButtons = (post) => (
     // TODO conditional render if auth
     <Button.Group>
       <Button primary>View</Button>
@@ -25,7 +33,7 @@ class Posts extends React.Component {
       <Button standard>Edit</Button>
       {/* TODO onclick edit Post  */}
       <Button.Or />
-      <Button color="red" inverted>
+      <Button color="red" onClick={() => this.deletePost(post.id)}>
         Delete
       </Button>
       {/* TODO onClick method to delete */}
@@ -35,15 +43,16 @@ class Posts extends React.Component {
   render() {
     const { posts } = this.state;
     return (
+      <>
+      <Link to={{pathname: '/blog/posts/new'}}>
+      <Button className='green'>New Post</Button>
+      </Link>
+      <br />
+      <br />
       <Card.Group itemsPerRow={4}>
         {posts.map(post => (
           <Card key={post.id}>
-            {/* TODO : make into links to Post.js show page */}
-            <Link
-              to={{
-                pathname: `/post/${post.id}`
-              }}
-            >
+            <Link to={{ pathname: `/post/${post.id}`}}>
               <Image src={post.image} />
             </Link>
             <Card.Content>
@@ -52,10 +61,11 @@ class Posts extends React.Component {
                 Published {this.timeFormat(post.created_at)}
               </Card.Meta>
             </Card.Content>
-            <Card.Content extra>{this.adminButtons()}</Card.Content>
+            <Card.Content extra>{this.adminButtons(post)}</Card.Content>
           </Card>
         ))}
       </Card.Group>
+      </>
     );
   }
 }
