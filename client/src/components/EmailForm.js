ddbf71222fb68516e9d2f6d46e10ac1bbec864b5
Willard Cron <willardcron@gmail.com>
2019-07-01 15:32:57 -0600

    emailForm text area/button

--- client/src/components/EmailForm.js
+++ client/src/components/EmailForm.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import { Form, Icon, } from "semantic-ui-react";
+import { Form, Icon, TextArea, Button, Container, Header } from "semantic-ui-react";
 
 class EmailForm extends React.Component {
 
   render () {
     return(
-      <Form action="https://formspree.io/email@domain.tld" method="POST">
+      <Container margin="50">
+        <Header as='h3'>We Would Love to Hear From You</Header>
+        <br />
+        <br />
+      <Form action="https://formspree.io/willardcron@gmail.com" method="POST">
       <Form.Group>
       <Form.Input
-        label="UserName"
+        width={3}
+        label="Name:"
         type="text"
         name="username"
+        placeholder="Your Name"
         // value={username}
-        width={4}
+        
         />
         <Form.Input
-        label="Email"
+        width={7}
+        label="Email:"
         type="email"
-        name="_replyto"
+        name="reply to"
+        placeholder="Your Email"
         // value={email}
-        width={8}
+        
         />
-      <Form.Input type="submit" value="Send"/>
       </Form.Group>
-    </Form>
+
+      <Form.Input
+        width={10}
+        control={TextArea}
+        label='Message:' 
+        name="body"
+        placeholder='What would you like to tell us...' 
+        />
+
+      <Form.Input control={Button} type="submit" icon>
+        <Icon name='send'/>
+        SEND
+        </Form.Input>
+      </Form>
+    </Container>
+      
 
     )
   }
