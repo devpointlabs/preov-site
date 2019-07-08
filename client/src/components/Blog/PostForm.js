2898c5b3137addbb198562770bbd2256184a6b0a
Nicki Nixon <nickitza@gmail.com>
2019-07-08 17:03:08 -0600

    post form post categories

--- client/src/components/Blog/PostForm.js
+++ client/src/components/Blog/PostForm.js
@@ -5,13 +5,14 @@ import ImageUploader from "react-images-upload";
 import { Link } from "react-router-dom";
 
 class PostForm extends React.Component {
-  state = { post: {
+  state = {
     title: "",
     body: "",
     image: "",
     categories: [],
-    post_categories: []}
+    post_categories: []
   };
+
   constructor(props) {
     super(props);
     this.onDrop = this.onDrop.bind(this);
@@ -23,49 +24,67 @@ class PostForm extends React.Component {
     if (post_id) {
       axios.get(`/api/posts/${post_id}`)
         .then(res => {
-        this.setState({ ...res.data });
+        this.setState({ ...res.data});
       });
-    } 
-    axios.get("/api/categories")
-     .then(res => {
-      this.setState({ post: { ...this.state.post, categories: res.data, } });
-      })
-      .catch(err => {
-        console.log("error in componentDidMount");
-      });
+    } else {
+      axios.get("/api/categories")
+       .then(res => {
+        this.setState({ ...this.state, categories: res.data, });
+        })
+        .catch(err => {
+          console.log("error in componentDidMount");
+        });
+    }
   }
 
-  handleChecked = (e, props) => {
-    const { post_categories } = this.state.post;
+  handleChecked = (e, target) => {
+    const { post_categories } = this.state;
+    // debugger
     //if props.checked is true
-    if (props.checked) {
+    if (target.checked) {
       //take what post_categories already is and add this category id
-      this.setState({ post: {...this.state.post, post_categories: [...post_categories, props.id]} });
-      //if props.checked is false (for unchecking a box)
+      this.setState({...this.state.post, post_categories: [...post_categories, { id: target.id }]});
+      //if target.checked is false (for unchecking a box)
     } else {
-      debugger
+
       //map through post_categories and return all where the id doesn't match the unchecked category id
-      let unchecked = post_categories.filter(cat => cat !== props.id);
-      // setState to not include props.id (remove it if it's there)
-      this.setState({ post: {...this.state.post, post_categories: unchecked} });
+      let unchecked = post_categories.filter(cat => {
+        // console.log(cat)
+        // console.log(target)
+        return cat.id !== target.id
+      });
+      
+      // setState to not include target.id (remove it if it's there)
+      this.setState({...this.state, post_categories: unchecked});
     }
   };
 
+  checked = (cat) => {
+    const { post_categories } = this.state;
+    let checked = false;
+    post_categories.map( c => {
+      if (c.id === cat)
+        checked = true;
+    })
+    return checked;
+  };
+
   categoryCheckboxes = () => {
-    const { categories, post_categories } = this.state.post;
-    return categories.map(cat => (
-      <Form.Checkbox
+    const { categories, post_categories } = this.state;
+    return categories.map(cat => {
+      return <Form.Checkbox
         key={cat.id}
         id={cat.id}
         name={cat.label}
         label={cat.label}
-        checked={post_categories.includes(cat.id)}
+        // checked={post_categories.includes(cat.id)}
+        checked={this.checked(cat.id)}
         //if post_categories includes category id, it will render a checked box
-        onChange={(e, props) => this.handleChecked(e, props)}
+        onChange={(e, target) => this.handleChecked(e, target)}
         /*when the checkbox is checked or unchecked (event/e), 
         it will pass the checkbox's props to handleChecked (we will use id and checked)*/
       />
-    ));
+    });
   };
 
   onDrop(image) {
@@ -74,16 +93,18 @@ class PostForm extends React.Component {
     });
   }
 
-  handleChange = (e) => { 
-    const {target: { name, value }} = e
-    this.setState({ [name]: value });
+  handleChange = (e, { name, value }) => { 
+    this.setState({ [name]: value } );
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+    const { post_categories, title, body, image, categories } = this.state
     let data = new FormData()
-    data.append('file', this.state.image)
-    data.append('categories', JSON.stringify(this.state.post_categories))
+    data.append('file', image)
+    data.append('categories', JSON.stringify(post_categories))
+    data.append("title", title);
+    data.append("body", body);
     //! how to update post
     const post_id = this.props.match.params.id;
     if(post_id){
@@ -96,7 +117,7 @@ class PostForm extends React.Component {
           console.log("error in handleSubmit")
         })
     }else{
-    axios.post(`/api/posts?title=${this.state.title}&body=${this.state.body}`, data)
+    axios.post(`/api/posts?title=${title}&body=${body}`, data)
     .then(res => {
       const {history } = this.props
       history.goBack()
@@ -112,6 +133,8 @@ class PostForm extends React.Component {
     return (
       <Form onSubmit={this.handleSubmit}>
         <Header>{this.props.match.params.id ? "Edit" : "New"}</Header>
+        {/* render the current image */}
+        {/* <img src={this.state.post.image} style={{ height: "300px", width: "300px"}} /> */}
         <ImageUploader
           withPreview={true}
           withIcon={true}
