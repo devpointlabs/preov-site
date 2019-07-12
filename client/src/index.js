06b9aa51a0865bd19c8f6067811a4b4b93e818f4
Nicki Nixon <nickitza@gmail.com>
2019-07-12 12:04:50 -0600

    remove serviceWorker, add initmiddleware

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import "semantic-ui-css/semantic.min.css"
 import { BrowserRouter, } from "react-router-dom"
 import { AuthProvider, } from "./providers/AuthProvider";
-import * as serviceWorker from './serviceWorker';
+import { initMiddleware, } from 'devise-axios'
+
+initMiddleware();
 
 ReactDOM.render(
  <AuthProvider>
