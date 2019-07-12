ad50c42794ab463ca1bfa9e20ee037c7cdc5ecef
Nicki Nixon <nickitza@gmail.com>
2019-07-12 15:58:51 -0600

    styling

--- client/src/components/Blog/Categories/Categories.js
+++ client/src/components/Blog/Categories/Categories.js
@@ -1,72 +1,73 @@
-import React from "react"
-import axios from "axios"
-import CategoryForm from "./CategoryForm"
-import CategoriesList from "./CategoriesList"
-import { Container, Header, Button } from "semantic-ui-react"
-import { Link, } from 'react-router-dom'
-import styled from 'styled-components'
+import React from "react";
+import axios from "axios";
+import CategoryForm from "./CategoryForm";
+import CategoriesList from "./CategoriesList";
+import { Container } from "semantic-ui-react";
+import styled from "styled-components";
 
 class Categories extends React.Component {
-  state = { categories: [], };
+  state = { categories: [] };
 
   componentDidMount() {
-    axios.get("/api/categories")
-      .then( res => {
-        this.setState({ categories: res.data, })
+    axios
+      .get("/api/categories")
+      .then(res => {
+        this.setState({ categories: res.data });
       })
-      .catch( err => {
+      .catch(err => {
         console.log(err);
-      })
-  }  
-
-  addCategory = (label) => {
-    axios.post("/api/categories", {label})
-      .then( res => {
-        const { categories, } = this.state;
-        this.setState({ categories: [...categories, res.data], });
-      })
+      });
   }
 
-  editCategory = (category) => {
-    axios.put(`/api/categories/${category.id}`, category)
-      .then( res => {
-        const categories = this.state.categories.map( cat => {
-          if (cat.id === category.id)
-            return res.data;
-          return cat;
-        });
-        this.setState({ categories, });
-      })
-  }
+  addCategory = label => {
+    axios.post("/api/categories", { label }).then(res => {
+      const { categories } = this.state;
+      this.setState({ categories: [...categories, res.data] });
+    });
+  };
 
-  deleteCategory = (id) => {
-    axios.delete(`/api/categories/${id}`)
-      .then( res => {
-        const { categories, } = this.state;
-        this.setState({ categories: categories.filter(cat => cat.id !==id), })
-      })
-  }
+  editCategory = category => {
+    axios.put(`/api/categories/${category.id}`, category).then(res => {
+      const categories = this.state.categories.map(cat => {
+        if (cat.id === category.id) return res.data;
+        return cat;
+      });
+      this.setState({ categories });
+    });
+  };
+
+  deleteCategory = id => {
+    axios.delete(`/api/categories/${id}`).then(res => {
+      const { categories } = this.state;
+      this.setState({ categories: categories.filter(cat => cat.id !== id) });
+    });
+  };
 
   render() {
     return (
-      <StyledContainer>
-        <br />
-        <CategoryForm addCategory={this.addCategory} />
-        <br />
-        <br />
-        <CategoriesList
-          categories={this.state.categories}
-          editCategory={this.editCategory}
-          deleteCategory={this.deleteCategory}
-        />
-      </StyledContainer>
-    )
+      <StyledDiv>
+        <StyledContainer>
+          <br />
+          <CategoryForm addCategory={this.addCategory} />
+          <br />
+          <br />
+          <CategoriesList
+            categories={this.state.categories}
+            editCategory={this.editCategory}
+            deleteCategory={this.deleteCategory}
+          />
+        </StyledContainer>
+      </StyledDiv>
+    );
   }
 }
 
-const StyledContainer = styled(Container)`
-  background-color: white;
-  padding: 1em 10em !important;
+const StyledDiv = styled.div`
 `
+
+const StyledContainer = styled(Container)`
+  background-color: #f8f8f8;
+  padding: 0em 10em !important;
+`;
 
 export default Categories;
